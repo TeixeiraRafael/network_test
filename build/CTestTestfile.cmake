19863bfbada0ed573ef6e59bf49c11495f2adb05
Rafael Teixeira <rodriguesteixeirarafael@gmail.com>
2021-04-28 10:10:50 -0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/network_test/src
+# Build directory: /home/user/network_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("chatter")
